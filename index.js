94cfa8d001b2f698ce4dadeee591005b7db986db
Felipe <luiztrd87@gmail.com>
2023-11-06 20:32:10 -0300

    formatação

--- index.js
+++ index.js
@@ -5,14 +5,14 @@ require("dotenv").config();
 
 //router
 
-const router = require('./routes/routes')
+const router = require("./routes/routes");
 
 //database
 const connection = require("./database/db");
 //porta para servidor
 PORT = process.env.PORT;
 
-app.use(express.static(__dirname +'/static'))
+app.use(express.static(__dirname + "/static"));
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
@@ -28,8 +28,7 @@ connection
     console.log(err + " falha ao conectar");
   });
 
-app.use('/', router)
-
+app.use("/", router);
 
 app.listen(PORT, () => {
   console.log("Server running..");
--- routes/routes.js
+++ routes/routes.js
@@ -1,49 +1,40 @@
-const express = require('express')
+const express = require("express");
 const bcrypt = require("bcrypt");
 
-const router = express.Router()
-const Paciente = require('../model/Paciente');
-
+const router = express.Router();
+const Paciente = require("../model/Paciente");
 
 router.get("/", (req, res) => {
-    res.render("formulario");
-  });
-
-
-
-
+  res.render("formulario");
+});
 
 router.post("/cadastro", async (req, res) => {
-    var senha = req.body.senha
-    if (senha) {
-      try{
-           var password = await bcrypt.hash(senha, 10);
-      }catch (error) {
-          console.error("Erro ao criar hash da senha: " + error);
-          return res.status(500).send("Erro ao criar hash da senha.");
-        }
-     
+  var senha = req.body.senha;
+  if (senha) {
+    try {
+      var password = await bcrypt.hash(senha, 10);
+    } catch (error) {
+      console.error("Erro ao criar hash da senha: " + error);
+      return res.status(500).send("Erro ao criar hash da senha.");
     }
-  
-    try{
-        await Paciente.create({
-  
+  }
+
+  try {
+    await Paciente.create({
       nome: req.body.nome,
       sobrenome: req.body.sobrenome,
       CPF: req.body.cpf,
       nascimento: req.body.nascimento,
-      senha: password
+      senha: password,
     })
       .then(() => console.log("Paciente criado."))
-      .then(()=>  res.redirect('/'))
+      .then(() => res.redirect("/"))
       .catch((err) => {
         console.log("Erro ao criaar novo paciente. " + err);
       });
-    }catch(err){
-      console.log(err + " erro ao criar paciente. ") 
-    }
-   
-  });
+  } catch (err) {
+    console.log(err + " erro ao criar paciente. ");
+  }
+});
 
-
-  module.exports = router;
+module.exports = router;
